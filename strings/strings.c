4b37864bed59a021ac0e9cc21a755ef509e9e809
cliffkang <cliffgkang@gmail.com>
2018-04-16 16:09:10 -0700

    finished strings

--- strings/strings.c
+++ strings/strings.c
@@ -7,7 +7,11 @@
 */
 int string_length(char s[])
 {
-
+    int count = 0;
+    while (s[count] != '\0') {
+        count++;
+    }
+    return count;
 }
 
 /*
@@ -20,7 +24,13 @@ int string_length(char s[])
 */
 char *reverse_string(char rv[], char s[])
 {
-
+    int count = 0;
+    for (int i = string_length(s) - 1; i >= 0; i--) {
+        rv[count] = s[i];
+        count++;
+    }
+    rv[count] = '\0';
+    return rv;
 }
 
 #ifndef TESTING
